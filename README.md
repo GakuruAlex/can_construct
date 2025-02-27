3259707aa1da2ff996c3920772995c961930ee4c
= <=>
2025-02-27 20:00:11 +0300

    program init

---
+++ README.md
@@ -0,0 +1,5 @@
+# CanConstruct #
+
+A program that accepts a target string and an array of strings. The program returns true or false based on whether the target string can be constructed by concatenating  strings in the array.
+
+Words in the array maybe reused as many times as possible.
---
+++ can_construct.py
@@ -0,0 +1,12 @@
+from typing import List
+def can_construct(target_word: str, word_bank: List[str])-> bool:
+    """_Find out whether a target word can be constructed by concatenating str in a given list _
+
+    Args:
+        target_word (str): _Word to construct_
+        word_bank (List[str]): _A list of str_
+
+    Returns:
+        bool: _True if word can be constructed from the given str else False_
+    """
+    ...
