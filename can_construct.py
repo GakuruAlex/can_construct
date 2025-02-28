8e84b2659a6293854a201c2ab2be1c4cdb7ca179
= <=>
2025-02-28 11:54:04 +0300

    can construct without memo

--- can_construct.py
+++ can_construct.py
@@ -9,4 +9,27 @@ def can_construct(target_word: str, word_bank: List[str])-> bool:
     Returns:
         bool: _True if word can be constructed from the given str else False_
     """
-    ...
+    if target_word  == "":
+        return True
+    for word in word_bank:
+            if target_word.startswith(word):
+                new_target_word = target_word.removeprefix(word)
+                if can_construct(target_word=new_target_word, word_bank=word_bank):
+                    return True
+    return False
+
+def main()-> None:
+    target_word: str = 'abcdef'
+    word_bank: List[str] = ['ab', 'abc', 'cd', 'def', 'abcd']
+
+    is_word_possible: bool = can_construct(target_word=target_word, word_bank=word_bank)
+
+    print(f"Is it possible to construct {target_word} by concatenating str(s) in {word_bank}? {is_word_possible}")
+    target_word: str = 'skateboard'
+    word_bank: List[str] = ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']
+
+    is_word_possible: bool = can_construct(target_word=target_word, word_bank=word_bank)
+    print(f"Is it possible to construct {target_word} by concatenating str(s) in {word_bank}? {is_word_possible}")
+
+if __name__ == "__main__":
+    main()
